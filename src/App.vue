288223ce2c346d38dcc1a778a04f84240d393e45
leehendrick <imhendrick16@gmail.com>
2025-02-15 00:36:14 -0500

    create navbar and customize home

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+  import { RouterLink, RouterView } from 'vue-router'
+  import SiteNavigation from "./components/SiteNavigation.vue"
 </script>
 
 <template>
-  <RouterView />
+  <div class="flex flex-col min-h-screen font-Roboto bg-weatherPrimary">
+    <SiteNavigation/>
+    <RouterView />
+  </div>
 </template>
  
 <style scoped>
