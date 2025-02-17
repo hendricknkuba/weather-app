5d065942a1e21bc3e9af208a9f032e48bc8a0e84
leehendrick <imhendrick16@gmail.com>
2025-02-17 15:09:23 -0500

    handling error

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,13 +11,18 @@
       <ul class="absolute bg-weatherSecondary text-white w-full shadow-md py-2 px-1 top-[66px]"
           v-if="mapbox_results"
       >
-        <li 
-          v-for="searchResult in mapbox_results" 
-          :key="searchResult.id" 
-          class="py-2 cursor-pointer"
-        >
-          {{ searchResult.place_name }}
-        </li>
+        <p v-if="searchError" class="text-red-700 font-bold ">Sorry, something went wrong, please try again.</p>
+        <p v-if="!serverError && mapbox_results.length === 0">No result match your query, try a different term.</p>
+
+        <template v-else>
+            <li 
+              v-for="searchResult in mapbox_results" 
+              :key="searchResult.id" 
+              class="py-2 cursor-pointer"
+            >
+            {{ searchResult.place_name }}
+          </li>
+        </template>
       </ul>
     </div>
   </main>
@@ -31,6 +36,7 @@
   const queryTimeOut = ref(null);
   const api_key = import.meta.env.VITE_API_KEY;
   const mapbox_results = ref(null);
+  const searchError = ref(null);
 
   const getSearchResults = () => {
 
@@ -38,11 +44,15 @@
     queryTimeOut.value = setTimeout(async () => {
 
       if (searchQuery.value !== ''){
-        const result = await axios.get(
-          `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${api_key}&types=place`
-        );
-        mapbox_results.value = result.data.features;
-        console.log(mapbox_results.value);
+
+        try{
+          const result = await axios.get(
+            `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${api_key}&types=place`
+          );
+          mapbox_results.value = result.data.features;
+        } catch {
+          searchError.value = true;
+        }
         return;
       }
       mapbox_results.value = null;
