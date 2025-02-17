a73df336a31ec2ec6c32f54f944bf4d44c50a6f2
leehendrick <imhendrick16@gmail.com>
2025-02-17 16:38:21 -0500

    handling async data

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import "./assets/tailwind.css";
-import BaseModal from "./components/BaseModal.vue"
+import BaseModal from "./components/BaseModal.vue";
 
 const app = createApp(App);
 
-app.component("BaseModal", BaseModal);
+app
+  .component("BaseModal", BaseModal)
+  .component("", );
 
 app.use(router);
 
--- src/views/CityView.vue
+++ src/views/CityView.vue
@@ -1,9 +1,14 @@
 <template>
   <div>
-
+    <Suspense>
+      <AsyncCityView/>
+      <template>
+        <p>Loading...</p>
+      </template>
+    </Suspense>
   </div>
 </template>
 
 <script setup>
-
+  import AsyncCityView from '@/components/AsyncCityView.vue';
 </script>
