91e48c13c3b2dc79abf12fd4452684baaa7167b2
leehendrick <imhendrick16@gmail.com>
2025-02-28 23:12:06 -0500

    removing unused imports

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import CityView from '../views/CityView.vue'
+import { defineAsyncComponent } from 'vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
