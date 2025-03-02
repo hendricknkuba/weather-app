a74ea1d9059f3b3e062223e26d801c3e457786c9
leehendrick <imhendrick16@gmail.com>
2025-03-02 00:09:41 -0500

    feat: rout transitions

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,23 @@
 <template>
   <div class="flex flex-col min-h-screen font-Roboto bg-weatherPrimary">
     <SiteNavigation/>
-    <RouterView />
+
+    <RouterView v-slot="{Component}">
+      <Transition name="page" mode="out-in">
+        <component :is="Component"/>
+      </Transition>
+    </RouterView>
   </div>
 </template>
  
-<style scoped>
+<style>
+   .page-enter-active,
+   .page-leave-active {
+      transition: 600ms ease all;
+   }
+
+   .page-enter-from,
+   .page-leave-to {
+    opacity: 0;
+   }
 </style>
